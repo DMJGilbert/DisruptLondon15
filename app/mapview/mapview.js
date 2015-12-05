a225aed3fe26878440ec2ea59f885aa4d0a1f372
Darren Gilbert <DMJGilbert@me.com>
2015-12-05 20:35:29 +0000

    Mapps

--- app/mapview/mapview.js
+++ app/mapview/mapview.js
@@ -1,20 +1,29 @@
 'use strict';
 
 angular.module('myApp.mapview', ['ngRoute', 'esri.map'])
-.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider.when('/mapview', {
-		templateUrl: 'mapview/mapview.html',
-		controller: 'SimpleMapCtrl'
-	});
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.when('/mapview', {
+			templateUrl: 'mapview/mapview.html',
+			controller: 'SimpleMapCtrl'
+		});
 }]).controller('SimpleMapCtrl', function ($scope) {
 
-    $scope.map = {
-		options: {
-			basemap: 'gray',
-			center: [-0.021,51.54],
-			zoom: 13,
-			sliderStyle: 'small'
+		$scope.map = {
+			options: {
+				basemap: 'gray',
+				center: [-0.021, 51.54],
+				zoom: 13,
+				sliderStyle: 'small'
+			}
+		};
+		$scope.mapLoaded = function (map) {
+
+			var text = new esri.symbol.TextSymbol("test").setColor(
+				new dojo.Color([10, 10, 10])).setAlign(esri.symbol.TextSymbol.ALIGN_START).setFont(
+				new esri.symbol.Font(((map.getZoom() * map.getZoom()) * .05) + "pt").setFamily('arial').setWeight(esri.symbol.Font.WEIGHT_BOLD));
+
+			var graphic = esri.Graphic(new esri.geometry.Point(-0.021, 51.54), text);
+			map.graphics.add(graphic)
 		}
-	};
-    
-});
+
+	});
