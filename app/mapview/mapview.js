2a28f46e7d251ac223508bd3116427b51171b9b7
Darren Gilbert <DMJGilbert@me.com>
2015-12-05 18:08:19 +0000

    Clean up and map!

--- app/mapview/mapview.js
+++ app/mapview/mapview.js
@@ -1,21 +1,18 @@
 'use strict';
 
-angular.module('myApp.mapview', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/mapview', {
-    templateUrl: 'mapview/mapview.html',
-    controller: 'SimpleMapCtrl'
-  });
-}])
-
-    .controller('SimpleMapCtrl', function($scope) {
-        $scope.map = {
-            options: {
-                basemap: 'topo',
-                center: [-122.45,37.75],
-                zoom: 13,
-                sliderStyle: 'small'
-            }
-        };
-    });
+angular.module('myApp.mapview', ['ngRoute', 'esri.map'])
+.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.when('/mapview', {
+		templateUrl: 'mapview/mapview.html',
+		controller: 'SimpleMapCtrl'
+	});
+}]).controller('SimpleMapCtrl', function ($scope) {
+	$scope.map = {
+		options: {
+			basemap: 'topo',
+			center: [-122.45, 37.75],
+			zoom: 13,
+			sliderStyle: 'small'
+		}
+	};
+});
