43ab146e9888cbcbdefd8c75477415dfa611c293
Marindrew <a@fj.by>
2015-12-06 03:55:37 +0000

    hackathonmode registration

--- app/signupview/signupview.js
+++ app/signupview/signupview.js
@@ -10,8 +10,15 @@ angular.module('myApp.signupview', ['ngRoute'])
 }])
 
 .controller('SignUpCtrl', function($scope, $location) {
+	
+	$scope.do_hackathon_mode = function() {
+		$scope.login = "CrazyDude";
+		$scope.password = "*************";
+		$scope.phoneNumber = "+447955585777";
+		$scope.actCode = "4563";
+	};
+	
     $scope.do_registration = function() {
-    		
     	register_with_twilio($scope.login,$scope.phoneNumber,$scope.password, function() {
     		USERNAME = $scope.login;
     		$scope.$apply(function (){
